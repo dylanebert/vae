dcb64909b3d180871c572b1fbe2898022a64360e
Dylan <dylan.ebert@gmail.com>
2018-07-09 15:41:34 -0400

    .

--- train.sh
+++ train.sh
@@ -3,4 +3,4 @@ source /home/debert/tensorflow-gpu/bin/activate
 export CUDA_HOME=/contrib/projects/cuda8.0
 export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:$LD_LIBRARY_PATH
 export PATH=${CUDA_HOME}/bin:${PATH}
-python3 -u /home/debert/vae/vae.py
+python3 /home/debert/vae/vae.py --data_path /home/debert/vae/data/ --save_path /home/debert/vae/model/ --train 100
