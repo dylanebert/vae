f7da39f11b9620e5e3b09b032eee313be01e35f4
dylan <dylan.ebert@gmail.com>
2018-09-03 11:20:39 -0400

    .

--- params.py
+++ params.py
@@ -2,7 +2,7 @@ class Params:
     def __init__(self):
         self.image_size = 64
         self.num_channels = 3
-        self.latent_size = 100
+        self.latent_size = 300
         self.learning_rate = .001
         self.batch_size = 100
         self.filters = 64
