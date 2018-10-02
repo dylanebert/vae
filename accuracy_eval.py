4aaad9506a20e1e3f81622873bdb27ba6756c40e
dylan <dylan.ebert@gmail.com>
2018-10-02 11:54:49 -0400

    .

--- accuracy_eval.py
+++ accuracy_eval.py
@@ -2,27 +2,24 @@ import numpy as np
 import pickle
 import os
 import json
-from config import Config
-from vae import VAE
-from data_generator import DataGenerator
 from scipy.spatial.distance import cosine, euclidean
 
 with open('model/gmc/means.p', 'rb') as f:
     means = pickle.load(f)
+with open('model/gmc/test_encodings.p', 'rb') as f:
+    encodings = pickle.load(f)
 
-config = Config()
-config.__dict__ = json.loads(open('model/gmc/config.json', 'r').read())
-
-test_generator = DataGenerator(config.test_path, config.image_size, config.batch_size)
-
-vae = VAE(config)
-encodings = vae.encoder.predict_generator(test_generator, verbose=1)
-print(encodings.shape)
-
+k = 0
+n = len(encodings)
 for label, encs in encodings.items():
-    break
+    k += 1
+    print('{0} of {1}'.format(k, n), end='\r')
     for i, enc in enumerate(encs):
-        nearest = list(dict(sorted(means.items(), key=lambda x: cosine(enc, x[1]))[:100]).keys())
-        line = json.dumps({'label': label, 'cos': cosine(enc, means[label]), 'euc': euclidean(enc, means[label]), 'predictions': nearest})
-        with open(os.path.join('model/gmc/predictions', label + '.json'), 'a+') as f:
-            f.write('{0}\n'.format(line))
+        if not os.path.exists(os.path.join('model/gmc/predictions', label + '.json')):
+            try:
+                nearest = list(dict(sorted(means.items(), key=lambda x: cosine(enc, x[1]))[:100]).keys())
+                line = json.dumps({'label': label, 'cos': cosine(enc, means[label]), 'euc': euclidean(enc, means[label]), 'predictions': nearest})
+                with open(os.path.join('model/gmc/predictions', label + '.json'), 'a+') as f:
+                    f.write('{0}\n'.format(line))
+            except:
+                print('Failed on {0}'.format(label))
