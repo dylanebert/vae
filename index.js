57cb541115e94b58f7abe878cb98a0762c32684b
Dylan <dylan.ebert@gmail.com>
2018-10-02 16:58:04 -0400

    .

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const encodingColors = {'c1': 'rgba(225, 0, 0, .2)', 'c2': 'rgba(0, 0, 225, .2)'
 
 var config = null
 var ctx = $('#plot')[0].getContext('2d')
+var chartData = {'c1': null, 'c2': null}
 var chart = new Chart(ctx, {
     type: 'scatter',
     data: {
@@ -47,81 +48,85 @@ var chart = new Chart(ctx, {
     }
 })
 
-function reload() {
-    $('.dropdown-item').click(function() {
-        var label = $(this).text()
-        var c = ''
-        if($(this).hasClass('c1')) {
-            c = 'c1'
-            $('#c2-title').text('')
-            $('#c2-reconstruction').attr('src', '')
-        } else {
-            c = 'c2'
-        }
-        $('#loadingScreen').css('display', '')
-        $('.row').css('display', '')
-        $('#loadingText').text('Loading class data')
-        $.get('http://localhost:5000/data?label=' + label, function(data) {
-            var parsed = $.parseJSON(data)
+function classClick(label, c) {
+    $('#loadingScreen').css('display', '')
+    $('#loadingText').text('Loading class data')
+    $.get('http://localhost:5000/data?label=' + label, function(data) {
+        var parsed = $.parseJSON(data)
+        var reconstruction_mean = parsed.img
+        var mean_encoding = parsed.mean
+        var encodings = parsed.encodings
 
-            $.each(['r1', 'r5', 'r10', 'r25', 'r50'], function(i, r) {
-                $.each(['an', 'cm'], function(j, s) {
-                    var idx = r + '-' + s
-                    var rVal = Math.round(parsed[idx] * 100) / 100
-                    $('#' + c + '-' + idx).text(rVal)
-                })
+        //Populate sidebar
+        $('#' + c + '-title').text(label)
+        $('#' + c + '-reconstruction-mean').attr('src', 'data:image/jpeg;base64, ' + reconstruction_mean)
+        $('#loadingScreen').css('display', 'none')
 
-            })
-
-            var img = parsed.img
-            var mean = parsed.mean
-            var encodings = parsed.encodings
-            $('#' + c + '-title').text(label)
-            $('#' + c + '-reconstruction').attr('src', 'data:image/jpeg;base64, ' + img)
-            $('#loadingScreen').css('display', 'none')
-            if(c == 'c1') {
-                while(chart.data.datasets.length > 0) {
-                    chart.data.datasets.pop()
+        //Populate accuracy table
+        $('#' + c + '-table-title').text(label + ' test accuracy:')
+        $.each(['r1', 'r5', 'r10', 'r25', 'r50'], function(i, r) {
+            $.each(['an', 'cm'], function(j, s) {
+                var idx = r + '-' + s
+                var rVal = Math.round(parsed[idx] * 100) / 100
+                var bgColor = 'rgba(231, 76, 60, 1)' //red
+                if(rVal >= .25) {
+                    bgColor = 'rgba(241, 196, 15, 1)' //yellow
                 }
-            } else {
-                if(chart.data.datasets.length > 1)
-                    chart.data.datasets.pop()
-            }
-            var bgColors = []
-            $.each(encodings, function(e) {
-                bgColors.push(encodingColors[c])
+                if(rVal >= .75) {
+                    bgColor = 'rgba(46, 204, 113, 1)' //green
+                }
+                $('#' + c + '-' + idx).text(rVal).css('background-color', bgColor)
             })
-            encodings.push(mean)
-            bgColors.push(meanColors[c])
-            chart.data.datasets.push({label: label, data: encodings, backgroundColor: encodingColors[c], pointBackgroundColor: bgColors})
-            chart.update()
         })
+
+        //Populate chart
+        var chartDatasets = []
+        var bgColors = []
+        $.each(encodings, function(e) { bgColors.push(encodingColors[c]); })
+        encodings.push(mean_encoding)
+        bgColors.push(meanColors[c])
+        chartData[c] = {label: label, data: encodings, backgroundColor: encodingColors[c], pointBackgroundColor: bgColors}
+        if(chartData['c1'] != null) {
+            chartDatasets.push(chartData['c1'])
+        }
+        if(chartData['c2'] != null) {
+            chartDatasets.push(chartData['c2'])
+        }
+        chart.data.datasets = chartDatasets
+        chart.update()
     })
 }
 
 function populateClasses() {
     $.get('http://localhost:5000/classes', function(json) {
         var data = $.parseJSON(json)
-        $('.dropdown-item').remove()
-        $('.helpOpen').remove()
-        $(data).each(function(i, entry) {
-            var html1 = '<li><a class="dropdown-item c1" href="#">' + entry + '</a></li>'
-            var html2 = '<li><a class="dropdown-item c2" href="#">' + entry + '</a></li>'
-            $('#dropdown-c1').append(html1)
-            $('#dropdown-c2').append(html2)
+        $.each(['c1', 'c2'], function(i, c) {
+            $('#' + c + '-autocomplete').autoComplete({
+                minChars: 1,
+                source: function(term, suggest) {
+                    term = term.toLowerCase()
+                    var choices = data
+                    var suggestions = []
+                    for (i = 0; i < choices.length; i++)
+                        if (~choices[i].toLowerCase().indexOf(term)) suggestions.push(choices[i])
+                    suggest(suggestions)
+                },
+                onSelect: function(e, term, item) {
+                    classClick(term, c)
+                }
+            })
         })
-        reload()
         $('#loadingScreen').css('display', 'none')
     })
 }
 
+//Model loading
 function loadModel() {
     dialog.showOpenDialog({ filters: [{ name: 'JSON', extensions: ['json'] }], properties: ['openFile'] }, function(filenames) {
         if(filenames == undefined) {
             console.log('No file selected')
             return
         }
-
         var filename = filenames[0]
         $('#loadingScreen').css('display', '')
         $('#loadingText').text('Loading model')
@@ -135,7 +140,6 @@ function loadModel() {
         })
     })
 }
-
 const template = [{
     label: 'File',
     submenu: [{
@@ -144,29 +148,10 @@ const template = [{
         }
     }]
 }]
-
 const menu = Menu.buildFromTemplate(template)
 Menu.setApplicationMenu(menu)
 
-$('.helpOpen').click(loadModel)
-
-//Dropdown
-$('.dropdown-search').keyup(function() {
-    const filter = $(this).val().toUpperCase()
-    $(this).parent().parent().find('.dropdown-item').each(function(i, elem) {
-        if($(elem).text().toUpperCase().indexOf(filter) > -1) {
-            $(elem).css('display', '')
-        } else {
-            $(elem).css('display', 'none')
-        }
-    })
-})
-
-$('.dropdown').on('hide.bs.dropdown', function() {
-    $('.dropdown-search').val('')
-    $('.dropdown-search').trigger('keyup')
-})
-
+//Check if model already loaded on server
 $(document).ready(function() {
     $('#loadingScreen').css('display', 'none')
     $.get('http://localhost:5000/is-loaded', function(data) {
