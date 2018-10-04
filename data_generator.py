3aff45913b5fe6a2b0923552314c7c421718d603
dylan <dylan.ebert@gmail.com>
2018-10-04 17:17:55 -0400

    .

--- data_generator.py
+++ data_generator.py
@@ -5,7 +5,7 @@ from keras.preprocessing.image import ImageDataGenerator
 class DataGenerator(keras.utils.Sequence):
     def __init__(self, path, image_size, batch_size):
         datagen = ImageDataGenerator(rescale=1./255)
-        self.generator = datagen.flow_from_directory(path, target_size=(image_size, image_size), color_mode='rgb', batch_size=batch_size, shuffle=True, class_mode='sparse')
+        self.generator = datagen.flow_from_directory(path, target_size=(image_size, image_size), color_mode='rgb', batch_size=batch_size, shuffle=False, class_mode='sparse')
 
     def __len__(self):
         return len(self.generator)
