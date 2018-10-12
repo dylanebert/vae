d0905cbe687efd572d85966085827c20b575e840
Dylan <dylan.ebert@gmail.com>
2018-10-12 13:39:44 -0400

    .

--- utils/crazy.py
+++ utils/crazy.py
@@ -3,22 +3,23 @@ import os
 
 with open('gmc_words', 'r') as f:
     gmc_words = f.read().splitlines()
+with open('wordnet_gmc_pairs', 'r') as f:
+    wordnet_gmc_pairs = [tuple(line.rstrip().split(' ')) for line in f]
 
-dir = 'model/gmc/predictions'
+dir = 'model/gmc/predictions_test'
 dirs = os.listdir(dir)
 n = len(dirs)
 for i, filename in enumerate(dirs):
     print('{0} of {1}'.format(i+1, n), end='\r')
     filepath = os.path.join(dir, filename)
     classifier_path = os.path.join('/home/dylan/Documents/inception/model/gmc/predictions', filename)
-    entailment_path = os.path.join('/data/entailment', filename.split('.')[0])
+    entailment_path = os.path.join('entailment_test', filename.split('.')[0])
     with open(filepath, 'r') as f:
-        with open(classifier_path, 'r') as h:
+        #with open(classifier_path, 'r') as h:
             with open(entailment_path, 'w+') as g:
                 g.write('FILENAME\tWORD\tENTAILS\tR1_COS\tR5_COS\tR10_COS\tR25_COS\tR50_COS\tR1_EUC\tR5_EUC\tR10_EUC\tR25_EUC\tR50_EUC\tR1_C\tR5_C\tR10_C\tR25_C\tR50_C\tP>.5\tP>.75\tP>.9\n')
                 filename_dict_vae = {}
                 filename_dict_classifier = {}
-                unified = []
                 for line in f:
                     entry = json.loads(line)
                     filename_dict_vae[entry['filename']] = entry
@@ -28,12 +29,15 @@ for i, filename in enumerate(dirs):
                 for filename, vae_entry in filename_dict_vae.items():
                     classifier_entry = filename_dict_classifier[filename]
                     for word in gmc_words:
+                        pair = (entry['label'], word)
+                        if pair not in wordnet_gmc_pairs and (pair[1], pair[0]) not in wordnet_gmc_pairs:
+                            continue
                         r1_cos = word in vae_entry['predictions_cos'][:1]
                         r5_cos = word in vae_entry['predictions_cos'][:5]
                         r10_cos = word in vae_entry['predictions_cos'][:10]
                         r25_cos = word in vae_entry['predictions_cos'][:25]
                         r50_cos = word in vae_entry['predictions_cos'][:50]
-                        r1_euc = word is vae_entry['predictions_euc'][0]
+                        r1_euc = word in vae_entry['predictions_euc'][:1]
                         r5_euc = word in vae_entry['predictions_euc'][:5]
                         r10_euc = word in vae_entry['predictions_euc'][:10]
                         r25_euc = word in vae_entry['predictions_euc'][:25]
