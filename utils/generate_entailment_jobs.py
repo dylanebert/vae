34a74b043279da6d525350caebf615e2e749d60a
dylan <dylan.ebert@gmail.com>
2018-10-25 15:13:01 -0400

    .

--- utils/generate_entailment_jobs.py
+++
@@ -1,9 +0,0 @@
-import os
-
-bash_str = '#!/bin/bash\ncd /data/nlp/vae\npython3 utils/entailment.py -i REPLACE -o /data/nlp/vae/entailment'
-
-for filepath in os.listdir('data/stratified'):
-    path = '/data/nlp/vae/data/stratified/' + filepath
-    bash_content = bash_str.replace('REPLACE', path)
-    with open(os.path.join('bash', filepath), 'w+') as f:
-        f.write(bash_content)
