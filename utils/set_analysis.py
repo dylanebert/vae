e795a5e003f726198208abdb7e853a11b444ded4
dylan <dylan.ebert@gmail.com>
2018-10-30 11:58:22 -0400

    .

--- utils/set_analysis.py
+++ utils/set_analysis.py
@@ -23,6 +23,8 @@ rows = ['S1_r1', 'S1_r5', 'S1_r10', 'S1_r25', 'S1_r50', 'S1_p5', 'S1_p75', 'S1_p
 
 n = len(wordnet_gmc_pairs)
 for i, (w1, w2) in enumerate(wordnet_gmc_pairs):
+    if os.path.exists(os.path.join('/data/nlp/vae/model/gmc/sets', w1 + '_' + w2)):
+        continue
     print('{0} of {1}: {2}, {3}'.format(i+1, n, w1, w2))
     table = {}
     for row in rows:
@@ -37,39 +39,43 @@ for i, (w1, w2) in enumerate(wordnet_gmc_pairs):
                     w1_members = f.read().splitlines()
 
                 for col_method in classifier_methods:
-                    w2_filepath = os.path.join(classifier_membership_dir, col_method, w2)
                     w2_members = []
                     precision = 0
                     recall = 0
+                    true_subset = False
                     col_header = col_method
+                    w2_filepath = os.path.join(classifier_membership_dir, col_method, w2)
                     if os.path.exists(w2_filepath):
                         with open(w2_filepath, 'r') as f:
                             w2_members = f.read().splitlines()
 
-                        true_positives = [member for member in w1_members if member in w2_members]
-                        precision = len(true_positives) / float(len(w1_members))
-                        recall = len(true_positives) / float(len(w2_members))
-                        true_subset = True
-                        for member in w1_members:
-                            if member not in w2_members:
-                                true_subset = False
+                        if len(w1_members) > 0 and len(w2_members) > 0:
+                            true_positives = [member for member in w1_members if member in w2_members]
+                            precision = len(true_positives) / float(len(w1_members))
+                            recall = len(true_positives) / float(len(w2_members))
+                            true_subset = True
+                            for member in w1_members:
+                                if member not in w2_members:
+                                    true_subset = False
 
                     table[row_header][col_header] = (precision, recall, int(true_subset))
     for dir in exemplar_subdirs:
         row_header = dir
-        with open(os.path.join(exemplar_dir, dir, w1), 'r') as f:
-            labels = []
-            p_vals = []
-            for line in f:
-                label, p_val = line.rstrip().split('\t')
-                labels.append(label)
-                p_vals.append(float(p_val))
-            for r in [1, 5, 10, 25, 50]:
-                col_header = 'r' + str(r)
-                table[row_header][col_header] = int(w2 in labels[:r])
-            for p in [.5, .75, .9]:
-                col_header = 'p' + str(p * 100).replace('.','').replace('0','')
-                table[row_header][col_header] = int(p_vals[labels.index(w2)] >= p)
+        exemplar_path = os.path.join(exemplar_dir, dir, w1)
+        if os.path.exists(exemplar_path):
+            with open(exemplar_path, 'r') as f:
+                labels = []
+                p_vals = []
+                for line in f:
+                    label, p_val = line.rstrip().split('\t')
+                    labels.append(label)
+                    p_vals.append(float(p_val))
+                for r in [1, 5, 10, 25, 50]:
+                    col_header = 'r' + str(r)
+                    table[row_header][col_header] = int(w2 in labels[:r])
+                for p in [.5, .75, .9]:
+                    col_header = 'p' + str(p * 100).replace('.','').replace('0','')
+                    table[row_header][col_header] = int(p_vals[labels.index(w2)] >= p)
     with open(os.path.join('/data/nlp/vae/model/gmc/sets', w1 + '_' + w2), 'w+') as f:
         f.write('\t'.join(['wordnet'] + cols) + '\n')
         for row in rows:
