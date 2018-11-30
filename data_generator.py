9ec0c94f14ee1b439cba3fffc4c8377ec3d0d22d
dylan <dylan.ebert@gmail.com>
2018-11-30 15:05:43 -0500

    .

--- data_generator.py
+++ data_generator.py
@@ -13,12 +13,3 @@ class DataGenerator(keras.utils.Sequence):
     def __getitem__(self, index):
         X, y = self.generator[index]
         return X, []
-
-if __name__ == '__main__':
-    #To test runtime
-    import datetime
-    print(datetime.datetime.now())
-    train_generator = DataGenerator('/data/nlp/gmc/train', 64, 100)
-    dev_generator = DataGenerator('/data/nlp/gmc/dev', 64, 100)
-    test_generator = DataGenerator('/data/nlp/gmc/test', 64, 100)
-    print(datetime.datetime.now())
