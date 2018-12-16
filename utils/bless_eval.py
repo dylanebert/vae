caff181aa43aeedc7bedf7bbebe86d0e6f2ed819
dylan <dylan.ebert@gmail.com>
2018-12-15 22:54:38 -0500

    .

---
+++ utils/bless_eval.py
@@ -0,0 +1,102 @@
+from metrics import Metrics
+import sys
+sys.path.append('/data/nlp/bless')
+from bless import Bless
+import pickle
+from tqdm import tqdm
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--input', help='inut encodings path', type=str, default='model/gmc/encodings.hdf5')
+parser.add_argument('-o', '--output', help='write detailed output to given file', type=str, default='model/gmc/tmp')
+parser.add_argument('--dispersion', action='store_true')
+parser.add_argument('--centroid', action='store_true')
+parser.add_argument('--entropy', action='store_true')
+parser.add_argument('--gaussian_random', action='store_true')
+parser.add_argument('--gaussian_dir', action='store_true')
+parser.add_argument('--test', action='store_true')
+args = parser.parse_args()
+
+bless = Bless()
+metrics = Metrics(args.input)
+words = list(metrics.encoding_word_indices.keys())
+
+if args.dispersion:
+    correct = 0
+    sum = 0
+    for i in tqdm(range(len(bless.pairs))):
+        w1, w2 = bless.pairs[i]
+        if w1 not in words or w2 not in words:
+            continue
+        if metrics.dispersion(w2) > metrics.dispersion(w1):
+            correct += 1
+        sum += 1
+    print('Dispersion: {0}'.format(correct / float(sum)))
+
+if args.centroid:
+    correct = 0
+    sum = 0
+    for i in tqdm(range(len(bless.pairs))):
+        w1, w2 = bless.pairs[i]
+        if w1 not in words or w2 not in words:
+            continue
+        if metrics.centroid(w2) > metrics.centroid(w1):
+            correct += 1
+        sum += 1
+    print('Centroid: {0}'.format(correct / float(sum)))
+
+if args.entropy:
+    correct = 0
+    sum = 0
+    for i in tqdm(range(len(bless.pairs))):
+        w1, w2 = bless.pairs[i]
+        if w1 not in words or w2 not in words:
+            continue
+        if metrics.entropy(w2) > metrics.entropy(w1):
+            correct += 1
+        sum += 1
+    print('Entropy: {0}'.format(correct / float(sum)))
+
+if args.gaussian_random:
+    correct = 0
+    sum = 0
+    for i in tqdm(range(len(bless.pairs))):
+        w1, w2 = bless.pairs[i]
+        if w1 not in words or w2 not in words:
+            continue
+        gauss_w1 = metrics.gaussian_random(w1)
+        gauss_w2 = metrics.gaussian_random(w2)
+        if gauss_w1 == -1 or gauss_w2 == -1:
+            continue
+        if gauss_w2 > gauss_w1:
+            correct += 1
+        sum += 1
+    print('Gaussian_random: {0}'.format(correct / float(sum)))
+
+if args.gaussian_dir:
+    correct = 0
+    sum = 0
+    with open(args.output, 'w+') as f:
+        for i in tqdm(range(len(bless.pairs))):
+            w1, w2 = bless.pairs[i]
+            if w1 not in words or w2 not in words:
+                continue
+            w1_w2_p = metrics.gaussian_dir(w1, w2)
+            w2_w1_p = metrics.gaussian_dir(w2, w1)
+            if w1_w2_p == -1 or w2_w1_p == -1:
+                continue
+            f.write('{0}\n'.format('\t'.join(str(e) for e in [w1, w2, w1_w2_p, w2_w1_p, w2_w1_p - w1_w2_p, int(w2_w1_p > w1_w2_p)])))
+            correct += w2_w1_p > w1_w2_p
+            sum += 1
+    print('Gaussian_dir: {0}'.format(correct / float(sum)))
+
+if args.test:
+    correct = 0
+    sum = 0
+    for i in tqdm(range(len(bless.pairs))):
+        w1, w2 = bless.pairs[i]
+        if w1 not in words or w2 not in words:
+            continue
+        correct += 0
+        sum += 1
+    print('test: {0}'.format(correct / float(sum)))
---
+++ utils/metrics.py
@@ -0,0 +1,91 @@
+import os
+import h5py
+import pickle
+from itertools import combinations
+from scipy.spatial.distance import cosine
+from scipy.stats import multivariate_normal
+import numpy as np
+
+class Metrics():
+    def __init__(self, encodings_path):
+        self.encodings_path = encodings_path
+        with open('model/gmc/encoding_word_indices.p', 'rb') as f:
+            self.encoding_word_indices = pickle.load(f)
+
+    def dispersion(self, word):
+        i, n = self.encoding_word_indices[word]
+        sum_cos = 0
+        with h5py.File(self.encodings_path) as f:
+            encodings = f['encodings'][i:i+n]
+            for x, y in combinations(encodings, 2):
+                sum_cos += cosine(x, y)
+        dispersion = (2 / (n * (n - 1))) * sum_cos
+        return dispersion
+
+    def centroid(self, word):
+        i, n = self.encoding_word_indices[word]
+        sum_dist = 0
+        with h5py.File(self.encodings_path) as f:
+            encodings = f['encodings'][i:i+n]
+            mean = np.mean(encodings, axis=0)
+            for encoding in encodings:
+                sum_dist += cosine(encoding, mean)
+        centroid = sum_dist / float(n)
+        return centroid
+
+    def entropy(self, word):
+        i, n = self.encoding_word_indices[word]
+        sum = 0
+        with h5py.File(self.encodings_path) as f:
+            encodings = f['encodings'][i:i+n]
+            mean = np.mean(encodings, axis=0)
+            for j in range(len(mean)):
+                p = np.abs(mean[j] / float(n))
+                sum += p * np.log2(p)
+        return -sum
+
+    def gaussian_random(self, word):
+        i, n = self.encoding_word_indices[word]
+        p_sum = 0
+        with h5py.File(self.encodings_path) as f:
+            encodings = f['encodings'][i:i+n]
+            l = encodings.shape[1]
+            if n < l:
+                return -1
+            mean = np.mean(encodings, axis=0)
+            cov = np.cov(encodings.T)
+            normal = multivariate_normal(mean=mean, cov=cov)
+            for v in np.random.normal(size=(1000, l)):
+                p = normal.pdf(v)
+                p_sum += p
+            return p_sum
+
+    def gaussian_dir(self, w1, w2): #returns probability that centroid of w2 is in distribution of w1
+        w1_i, w1_n = self.encoding_word_indices[w1]
+        w2_i, w2_n = self.encoding_word_indices[w2]
+        with h5py.File(self.encodings_path) as f:
+            w1_encodings = f['encodings'][w1_i:w1_i+w1_n]
+            w2_encodings = f['encodings'][w2_i:w2_i+w2_n]
+            l = w1_encodings.shape[1]
+            if w1_n < l or w2_n < l:
+                return -1
+            w1_mean = np.mean(w1_encodings, axis=0)
+            w2_mean = np.mean(w2_encodings, axis=0)
+            w1_cov = np.cov(w1_encodings.T)
+            w1_normal = multivariate_normal(mean=w1_mean, cov=w1_cov)
+            return w1_normal.pdf(w2_mean)
+
+if __name__ == '__main__':
+    metrics = Metrics('model/gmc/encodings.hdf5')
+    print(metrics.dispersion('dog'))
+    print(metrics.dispersion('animal'))
+    print(metrics.dispersion('person'))
+    print(metrics.dispersion('entity'))
+    print(metrics.centroid('dog'))
+    print(metrics.centroid('animal'))
+    print(metrics.centroid('person'))
+    print(metrics.centroid('entity'))
+    print(metrics.entropy('dog'))
+    print(metrics.entropy('animal'))
+    print(metrics.entropy('person'))
+    print(metrics.entropy('entity'))
